d804f844dce0f61f233c7681f466a8f32fbd0c95
isabellpaulmann <104442412+isabellpaulmann@users.noreply.github.com>
2022-09-08 09:03:10 +0200

    add global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from 'styled-components'
+
+const styled = {createGlobalStyle}
+
+export default styled.createGlobalStyle`
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    background-color: #333
+}`
