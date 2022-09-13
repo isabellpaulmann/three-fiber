05f7b5d4b0573126c654004cca7279520371b52e
isabellpaulmann <104442412+isabellpaulmann@users.noreply.github.com>
2022-09-13 12:19:23 +0200

    add star background

--- src/SimpleBox.js
+++ src/SimpleBox.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
 import { Canvas } from '@react-three/fiber';
+import { Stars } from '@react-three/drei';
 
 export default function SimpleBox() {
     return (
         <StyledCanvas>
          <Canvas camera={{ position: [5, 2, 2] }}>
+          <Stars
+          radius={130}
+          depth={90}
+          count={8000}
+          factor={3}
+                    />
           <pointLight position={[3, 5, 2]} intensity={4} color='white' />
           <mesh>
             <boxBufferGeometry args={[2, 2, 2]} />
@@ -18,4 +25,5 @@ export default function SimpleBox() {
 
 const StyledCanvas = styled.div`
 height: 600px;
+background: #111;
 `
